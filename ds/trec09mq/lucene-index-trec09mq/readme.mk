fdd00fbb60c4412363e82de3711c9118c49bbf95
ZahraTaherikhonakdar <75437827+ZahraTaherikhonakdar@users.noreply.github.com>
2022-06-17 18:15:11 -0400

    Update readme.mk

--- ds/trec09mq/lucene-index-trec09mq/readme.mk
+++ ds/trec09mq/lucene-index-trec09mq/readme.mk
@@ -1,1 +1,5 @@
 trec2009MQ is based on ClueWeb2009. So, the ClueWeb2009's index should be used.
+
+```
+$> anserini/target/appassembler/bin/IndexCollection -collection ClueWeb09Collection -input ClueWeb09-B-Corpus -index lucene-index.cw09b.pos+docvectors+rawdocs -generator JsoupGenerator -threads 44 -storePositions -storeDocvectors -storeRawDocs 2>&1 | tee  log.cw09b.pos+docvectors+rawdocs &
+```
