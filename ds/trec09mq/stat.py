6b0fcd4575de784e52a44ddfc0bdf213e6c486e2
DESKTOP-4HIM1FG\Zahra <zahrat509@gmail.com>
2022-07-21 13:36:49 -0400

    no message

--- ds/trec09mq/stat.py
+++ ds/trec09mq/stat.py
@@ -1,31 +1,34 @@
 import pandas as pd
+import pickle
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def read_refinements(all, dataset, querytype):
-
-    matrix = [[0 for x in range(col)] for y in range(row)]
-    for q in range(len(result)):
-    df_all=pd.read_csv(all, encoding="ISO-8859-1", sep=',')
+    df_all = pd.read_csv(all, encoding="ISO-8859-1", sep=',')
     R = pd.read_csv(all, encoding="ISO-8859-1", sep=',').columns[4:len(df_all.columns):3]
-    df_results= pd.read_csv(dataset, encoding="ISO-8859-1", sep=',')
-    df_qt=pd.read_csv(querytype, encoding="ISO-8859-1", sep='\t',usecols =['Topic', 'Class'])
-    df_qt['Topic']=df_qt['Topic'].astype(int)
+    df_results = pd.read_csv(dataset, encoding="ISO-8859-1", sep=',')
+    df_qt = pd.read_csv(querytype, encoding="ISO-8859-1", sep='\t', usecols=['Topic', 'Class'])
+    df_qt['Topic'] = df_qt['Topic'].astype(int)
     df_qt.rename(columns=({'Topic': 'qid'}), inplace=True)
     df = pd.merge(df_results, df_qt, on='qid', how='inner')
     QT = df_qt['Class'].unique()
 
-    A=dict()
+    A = dict()
     for qt in QT:
-        A[qt]=dict()
-        for r in R: A[qt][r]=0
+        A[qt] = dict()
+        for r in R: A[qt][r] = 0
 
-    for index,row in df.iterrows():
-        r_n=row['star_model_count']*3
-        for i in range(1,r_n,3):
-            r=str(row[i+3])
-            qt=row["Class"]
-            A[qt][r] +=1
+    for index, row in df.iterrows():
+        r_n = row['star_model_count'] * 3
+        for i in range(1, r_n, 3):
+            r = str(row[i + 3])
+            qt = row["Class"]
+            A[qt][r] += 1
 
     print(A)
 
+
 if __name__ == "__main__":
-    read_refinements('../../qe/output/trec09mq/topics.trec09mq.bm25.map.all.csv','../../qe/output/trec09mq/topics.trec09mq.bm25.map.dataset.csv','./queryclasses')
+    read_refinements('../../qe/output/trec09mq/topics.trec09mq.bm25.map.all.csv',
+                     '../../qe/output/trec09mq/topics.trec09mq.bm25.map.dataset.csv', './queryclasses')
