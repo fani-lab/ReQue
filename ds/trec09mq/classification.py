bb7c37e5e6309feae442b05f504ace13632ad431
DESKTOP-4HIM1FG\Zahra <zahrat509@gmail.com>
2022-08-10 19:33:46 -0400

    classification update #22

--- ds/trec09mq/classification.py
+++ ds/trec09mq/classification.py
@@ -1,13 +1,17 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import svm
 from sklearn.model_selection import KFold, train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import f1_score, roc_auc_score
 import pandas as pd
 import numpy as np
 import dal
 import extract_features as ef
+import os, traceback, math, threading, time
+from joblib import dump, load
+import sys
+import torch #just for topk(1)! should be replaced.
+sys.path.extend(["../"])
+import multiprocessing
 
 def preprocess_data(dataset, qtypes):
     df_results= pd.read_csv(dataset, sep=',')
@@ -31,26 +35,105 @@ def create_evaluation_splits(n_sample, n_folds):
 
     return splits
 
+def calc_metrics(y, y_, probs):
+    metrics = dict()
+    metrics['macro_auc_ovo'] = metrics['weighted_auc_ovo'] = metrics['macro_auc_ovr'] = metrics['weighted_auc_ovr'] = metrics['weighted_f1'] = None
+
+    try:
+        metrics['weighted_f1'] = f1_score(y_, y, average='weighted')
+        metrics['macro_auc_ovo'] = roc_auc_score(y, probs, multi_class="ovo", average="macro")
+        metrics['weighted_auc_ovo'] = roc_auc_score(y, probs, multi_class="ovo", average="weighted")
+    except:
+        print(traceback.format_exc())
+        pass
+    return metrics
+
+def log_metrics(metrics, output):
+    with open(output, 'w') as f:
+        f.write('fold#,')
+        header = 0
+        for fold, values in metrics.items():
+            if header == 0:
+                for metric_name in metrics[fold].keys():
+                    f.write(f'{metric_name},')
+                f.write('\n')
+                header = 1
+            f.write(f'{fold},')
+            for value in values.values():
+                f.write(f'{value},')
+            f.write('\n')
+
+def learn_kf(X, y, model, splits, Q, output):
+    metrics = {}
+    for foldidx in splits['folds'].keys():
+        X_train = X[splits['folds'][foldidx]['train'], :]
+        y_train = y[splits['folds'][foldidx]['train']]
+        # X_valid = X[splits['folds'][foldidx]['valid'], :]
+        # y_valid = y[splits['folds'][foldidx]['valid']]
+
+
+        model.fit(X_train, y_train)
+        if not os.path.isdir(f'{output}'): os.makedirs(f'{output}')
+        dump(model, f'{output}/{model.__class__.__name__}.train.fold{foldidx}.pkl')
+        # probs = model.predict_proba(X_valid)
+
+        # _, idxes = torch.as_tensor(model.predict_proba(X_valid)).topk(1)
+        # y_pred = [model.classes_[i] for i in idxes]
+        # metrics[foldidx] = calc_metrics(y_valid, y_pred, probs.max(axis=1))
+        #df = pd.DataFrame(np.asarray([Q.loc[Q.index[splits['folds'][foldidx]['valid']]]['Original Query'], splits['folds'][foldidx]['valid'], y_valid, y_pred]).transpose(), columns=['List of Terms', 'idx', 'true', 'pred'], dtype=object)
+        df.to_csv(f'{output}/{model.__class__.__name__}.valid{foldidx}.pred.csv', index=False)
+
+    log_metrics(metrics, f'{output}/{model.__class__.__name__}.valid.log')
+
+def test(X, y, model, splits, Q, output):
+    metrics = dict()
+    X_test = X[splits['test'], :]
+    y_test = y[splits['test']]
+
+    all_train = np.concatenate([splits['folds'][0]['train'], splits['folds'][0]['valid']])
+    X_train = X[all_train, :]
+    y_train = y[all_train]
+    model.fit(X_train, y_train)
+    if not os.path.isdir(f'{output}'): os.makedirs(f'{output}')
+    dump(model, f'{output}/{model.__class__.__name__}.train.fold_.pkl')
+    probs = model.predict_proba(X_test)
+    _, idxes = torch.as_tensor(model.predict_proba(X_test)).topk(1)
+    y_pred = [model.classes_[i] for i in idxes]
+    metrics['_'] = calc_metrics(y_test, y_pred, probs.max(axis=1))
+    df = pd.DataFrame(np.asarray([Q.loc[Q.index[splits['test']]]['Original Query'], splits['test'],y_test, y_pred]).transpose(), columns=['List of Terms', 'idx', 'true', 'pred'], dtype=object)
+    df.to_csv(f'{output}/{model.__class__.__name__}.test_.pred.csv', index=False)
+
+    for foldidx in splits['folds'].keys():
+        model = load(f'{output}/{model.__class__.__name__}.train.fold{foldidx}.pkl')
+        probs = model.predict_proba(X_test)
+        _, idxes = torch.as_tensor(model.predict_proba(X_test)).topk(1)
+        y_pred = [model.classes_[i] for i in idxes]
+        metrics[foldidx] = calc_metrics(y_test, y_pred, probs.max(axis=1))
+        df = pd.DataFrame(np.asarray([Q.loc[Q.index[splits['test']]]['Original Query'], splits['test'],y_test, y_pred]).transpose(), columns=['List of Terms', 'idx', 'true', 'pred'], dtype=object)
+        df.to_csv(f'{output}/{model.__class__.__name__}.test{foldidx}.pred.csv', index=False)
+
+    log_metrics(metrics, f'{output}/{model.__class__.__name__}.test.log')
+
 def main(splits, Q, y,feature_set, path, cmd=['prep', 'train', 'eval', 'test']):
     q_features, q_labels = ef.extract_load_q_features(Q, y,feature_set, q_features_file=path)
-    #print(q_features)
+    print(q_features)
 #     if 'userid' in feature_set:
 #         userID_col = np.array(Q['UserID'])[:,None]
 #         q_features = sparse.hstack([userID_col, q_features]).tocsr()
 #
-#     cores = multiprocessing.cpu_count()
-#     models = [LogisticRegression(n_jobs=cores, max_iter=100, random_state=0),
-#               RandomForestClassifier(n_jobs=cores, n_estimators=100, max_depth=10, random_state=0),  #
-#               # XGBClassifier(n_jobs=cores, n_estimators=1000, max_depth=15, learning_rate=0.1, random_state=random_seed),
-#               # MLP (word embdedding in input, n_refiners in output)
-#               # multilabel
-#               ]
-#
-#     output = f'{path}.qf'
-#
-#     for model in models:
-#         if 'train' in cmd: learn_kf(q_features, q_labels.toarray().ravel(), model, splits=splits, Q=Q, output=output)
-#         if 'test' in cmd: test(q_features, q_labels.toarray().ravel(), model, splits=splits, Q=Q, output=output)
+    cores = multiprocessing.cpu_count()
+    models = [LogisticRegression(n_jobs=cores, max_iter=100, random_state=0),
+              RandomForestClassifier(n_jobs=cores, n_estimators=100, max_depth=10, random_state=0),  #
+              # XGBClassifier(n_jobs=cores, n_estimators=1000, max_depth=15, learning_rate=0.1, random_state=random_seed),
+              # MLP (word embdedding in input, n_refiners in output)
+              # multilabel
+              ]
+
+    output = f'{path}.qf'
+
+    for model in models:
+        if 'train' in cmd: learn_kf(q_features, q_labels.toarray().ravel(), model, splits=splits, Q=Q, output=output)
+       # if 'test' in cmd: test(q_features, q_labels.toarray().ravel(), model, splits=splits, Q=Q, output=output)
 
 
 def con_r2i(df,all):
@@ -87,24 +170,18 @@ def con_t2i(df):
     # the df contain both refinemnets and qtypes indexes
     return df
 
-
-
-
 if __name__ == "__main__":
-    df_results=preprocess_data('../../qe/output/trec09mq/topics.trec09mq.bm25.map.dataset.csv',
-                    './queryclasses')
+    df_results=preprocess_data('../../qe/output/trec09mq/topics.trec09mq.bm25.map.dataset.csv','./queryclasses')
     splits = create_evaluation_splits(len(df_results), 5)
     feature_sets = [['basic'], ['userid', 'basic']]
     con_r2i(df_results,'../../qe/output/trec09mq/topics.trec09mq.bm25.map.all.csv')
     #the df contain both refinemnets and qtypes indexes
     df=con_t2i(con_r2i(df_results,'../../qe/output/trec09mq/topics.trec09mq.bm25.map.all.csv'))
-    print("df",df['method.1'])
-    feature_sets = [['basic'], ['userid', 'basic']]
+    #feature_sets = [['w2v'], ['bert'], ['w2v','bert'], ['userid', 'w2v'], ['userid', 'bert'], ['userid', 'w2v','bert']]
+    feature_sets = [['bert'], ['userid', 'bert']]
     for feature_set in feature_sets:
         feature_set_str = '.'.join(feature_set)
-    main(splits, df['abstractqueryexpansion'], df['method.1'],feature_sets, f'./results')
-
-
+        main(splits, df['abstractqueryexpansion'], df['method.1'],feature_set, f'./results.npz')
 
     # try:
     #     features_label = utils.load_sparse_csr(q_features_file)
--- ds/trec09mq/extract_features.py
+++ ds/trec09mq/extract_features.py
@@ -2,16 +2,15 @@ from scipy import sparse
 import pandas as pd
 import numpy as np
 import os
-
+from sentence_transformers import SentenceTransformer
 from lib import text_corpus as tc, utils
 
 def extract_features(Q, feature_set=[]):#['basic', 'linguistic', 'w2v', 'bert']
     char_ngram_range = (1, 1)
     word_ngram_range = (1, 1)
-    tc_Q = tc.TextCorpus(Q, word_pattern=r"[0-9]+", char_ngram_range=char_ngram_range, word_ngram_range=word_ngram_range)
+    tc_Q = tc.TextCorpus(Q, char_ngram_range=char_ngram_range, word_ngram_range=word_ngram_range)
 
     q_features = sparse.csr_matrix(tc_Q.getLengthsByTerm()).transpose()
-    print('tc_Q',tc_Q)
 
     if 'basic' in feature_set: q_features = sparse.csr_matrix(sparse.hstack((
         q_features,
@@ -20,30 +19,35 @@ def extract_features(Q, feature_set=[]):#['basic', 'linguistic', 'w2v', 'bert']
         tc_Q.getTfIdF(),
         # tc_Q.getCharBM25(),
         # tc_Q.getTermBM25()
+
     )))
-    #print("getTermStat", tc_Q.getTermStat()[0])
-    print("getTfIdF", tc_Q.getTfIdF())
+    if 'w2v' in feature_set:
+        model = SentenceTransformer('average_word_embeddings_glove.6B.300d')
+        sentence_embeddings = model.encode(Q.values)
+        q_features = sparse.csr_matrix(sparse.hstack((q_features, sentence_embeddings)))
+    if 'bert' in feature_set:
+        model = SentenceTransformer('paraphrase-distilroberta-base-v2')
+        sentence_embeddings = model.encode(Q.values)
+        q_features = sparse.csr_matrix(sparse.hstack((q_features, sentence_embeddings)))
 
     return q_features
 
+
 def extract_load_q_features(Q, y, feature_set, q_features_file=None):
-    print("kkkkkkkk",y)
     try:
         features_label = utils.load_sparse_csr(q_features_file)
         x = features_label[:, :-1]
         y = features_label[:, -1:]
-        print("lllllllllllllllll",x)
+        return x, y
     except:
         x = extract_features(Q, feature_set)
+        y = np.array([y], dtype=float).transpose()
         print(x.shape, y.shape)
         # print(x)
         # print(y.values.reshape(y.shape[0],1))
-        x_y = sparse.csr_matrix(sparse.hstack((x, y.values.reshape(y.shape[0],1))))
-        #rint("llpppppppppppp", x_y)
-        #utils.save_sparse_csr(q_features_file, x_y)
-
-
-
+        x_y = sparse.csr_matrix(sparse.hstack((x, y)))
+        utils.save_sparse_csr(q_features_file, x_y)
+        return x, y
 
     # try:
     #     features_label = utils.load_sparse_csr(q_features_file)
