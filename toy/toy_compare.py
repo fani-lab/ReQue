d626b3cba77cf35a6c7d5fbf3e183fe51ea2699f
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2023-06-16 20:02:48 -0400

    Update toy_compare.py

--- toy/toy_compare.py
+++ toy/toy_compare.py
@@ -20,7 +20,7 @@ def calculate_tValue_cohensD(map1, map2):
     return t_value, cohens_d
 
 
-def compare_mAP_each_row(infile_df, outfile_path, columns):
+def compare_mAP_each_row(infile_df, outfile_path, col1, col2):
     # Header
     headers = list(infile_df.columns)
     headers.append('t_value')
@@ -36,8 +36,8 @@ def compare_mAP_each_row(infile_df, outfile_path, columns):
     for index, row in infile_df.iterrows():
 
         # mAP for the two expanders
-        map1 = row[[word for word in map_columns if word.startswith(columns.col1)][0]]
-        map2 = row[[word for word in map_columns if word.startswith(columns.col2)][0]]
+        map1 = row[[word for word in map_columns if word.startswith(col1)][0]]
+        map2 = row[[word for word in map_columns if word.startswith(col2)][0]]
 
         # Check empty cells
         t_value, cohens_d = calculate_tValue_cohensD(map1=map1, map2=map2)
@@ -45,17 +45,19 @@ def compare_mAP_each_row(infile_df, outfile_path, columns):
         # New row of the output file csv file
         new_line=[]
         new_line.append(row.qid)  # qid
-        new_line.append(row[columns.col1])  # qid
+        new_line.append(row[col1])  # qid
         new_line.append(map1)  # col 1 mAP
-        new_line.append(row[columns.col2])  # col 2 mAP
+        new_line.append(row[col2])  # col 2 mAP
         new_line.append(map2)  # col 2 mAP
         new_line.append(t_value)  # t_value
         new_line.append(cohens_d)  # cohens_d
 
         csv_result.loc[len(csv_result)] = new_line
 
+    #TODO Write another new excel just one time with extra col
+
     # write dataFrame to SalesRecords CSV file
-    output_file_name = f'compare.mAP.{columns.col1}.{columns.col2}.csv'
+    output_file_name = f'compare.mAP.{col1}.{col2}.csv'
     csv_result.to_csv(outfile_path + output_file_name)
 
 
@@ -67,26 +69,27 @@ def plot_result(map1, map2):
     plt.show()
 
 
-def compare_mAP_all_row(infile_df, outfile_path, columns):
+def compare_mAP_all_row(infile_df, outfile_path, col1, col2):
     headers = list(infile_df.columns)
     map_columns = [word for word in headers if word.endswith("map")]
-    map1_header = [word for word in map_columns if word.startswith(columns.col1)][0]
-    map2_header = [word for word in map_columns if word.startswith(columns.col2)][0]
+    map1_header = [word for word in map_columns if word.startswith(col1)][0]
+    map2_header = [word for word in map_columns if word.startswith(col2)][0]
 
     infile_df.dropna(subset=[map1_header, map2_header])
 
-    map1 = infile_df[[word for word in map_columns if word.startswith(columns.col1)][0]].tolist()
+    map1 = infile_df[[word for word in map_columns if word.startswith(col1)][0]].tolist()
     map1 = [0 if str(item) == 'nan' else item for item in map1]
-    map2 = infile_df[[word for word in map_columns if word.startswith(columns.col2)][0]].tolist()
+    map2 = infile_df[[word for word in map_columns if word.startswith(col2)][0]].tolist()
     map2 = [0 if str(item) == 'nan' else item for item in map2]
 
     mean1 = np.mean(map1)
     mean2 = np.mean(map2)
 
-    output_file_name = f'compare.mAP.{columns.col1}.{columns.col2}.txt'
+    return mean1, mean2
+    output_file_name = f'compare.mAP.{col1}.{col2}.txt'
     with open(outfile_path+output_file_name, 'w') as file:
-        file.write(f'{columns.col1} mean of mAP is: {mean1}\n')
-        file.write(f'{columns.col2} mean of mAP is: {mean2}\n')
+        file.write(f'{col1} mean of mAP is: {mean1}\n')
+        file.write(f'{col2} mean of mAP is: {mean2}\n')
 
 
 def get_input():
@@ -114,8 +117,9 @@ if __name__ == '__main__':
     infile_df = pd.read_csv(infile_path)
     infile_df = infile_df.reset_index()  # making sure indexes pair with number of rows
 
-    compare_mAP_each_row(infile_df=infile_df, outfile_path=outfile_path, columns=columns)
-    compare_mAP_all_row(infile_df=infile_df, outfile_path=outfile_path, columns=columns)
+    for index, each_col in enumerate(columns.col2):
+        compare_mAP_each_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=each_col)
+        compare_mAP_all_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=each_col)
 
 
 
