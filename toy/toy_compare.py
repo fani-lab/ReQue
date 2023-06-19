27a892e794f4378c87364b85efe5edefddd7fa9f
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2023-06-19 18:22:06 -0400

    Update toy_compare.py
    Add multi-column comparison
    Add plotting results

--- toy/toy_compare.py
+++ toy/toy_compare.py
@@ -21,75 +21,67 @@ def calculate_tValue_cohensD(map1, map2):
 
 
 def compare_mAP_each_row(infile_df, outfile_path, col1, col2):
-    # Header
-    headers = list(infile_df.columns)
-    headers.append('t_value')
-    headers.append('cohens_d')
-    headers.remove('index')
-    headers = [x for x in headers if not re.match(r"^qid.*\d|\w$", x)]
+    # Initialize header
+    headers = ['qid']
+    col2_list = []
+    for word in list(infile_df.columns):
+        if word.startswith(col2) and word.endswith("map"):
+            headers.append(word)
+            headers.append('t-value')
+            headers.append('cohens-d')
+            col2_list.append(word)
+        elif word.startswith(col1) and word.endswith("map"):
+            # Make sure the main col that wants to do the comparison with, is placed at the second col.
+            # The first column that match the col1 input is considered for main col.
+            headers.insert(1, word)
+            col1 = word
 
-    # Output file dataframe
+    # Output csv file
     csv_result = pd.DataFrame(columns=headers)
 
-    map_columns = [word for word in headers if word.endswith("map")]
-
     for index, row in infile_df.iterrows():
+        # Initialize new line
+        # If a list of names, starting with col1, is returned, only the first one is considered!
+        map1 = row[col1]
+        new_line = [row.qid, map1]
 
-        # mAP for the two expanders
-        map1 = row[[word for word in map_columns if word.startswith(col1)][0]]
-        map2 = row[[word for word in map_columns if word.startswith(col2)][0]]
+        for index, map2 in enumerate(col2_list):
+            # Check empty cells
+            t_value, cohens_d = calculate_tValue_cohensD(map1=map1, map2=row[map2])
 
-        # Check empty cells
-        t_value, cohens_d = calculate_tValue_cohensD(map1=map1, map2=map2)
-
-        # New row of the output file csv file
-        new_line=[]
-        new_line.append(row.qid)  # qid
-        new_line.append(row[col1])  # qid
-        new_line.append(map1)  # col 1 mAP
-        new_line.append(row[col2])  # col 2 mAP
-        new_line.append(map2)  # col 2 mAP
-        new_line.append(t_value)  # t_value
-        new_line.append(cohens_d)  # cohens_d
+            # New row of the output file csv file
+            new_line.append(row[map2])  # col 2 mAP
+            new_line.append(t_value)  # t_value
+            new_line.append(cohens_d)  # cohens_d
 
         csv_result.loc[len(csv_result)] = new_line
 
-    #TODO Write another new excel just one time with extra col
-
-    # write dataFrame to SalesRecords CSV file
-    output_file_name = f'compare.mAP.{col1}.{col2}.csv'
-    csv_result.to_csv(outfile_path + output_file_name)
+    # write dataFrame to a CSV file
+    csv_result.to_csv(outfile_path + '.csv')
 
 
-def plot_result(map1, map2):
-    xpoints = np.array([1, 8])
-    ypoints = np.array([3, 10])
+def plot_result(infile_df, col1, col2):
+    # Map of the main column
+    map1 = infile_df[
+        [word for word in list(infile_df.columns) if word.startswith(col1) and word.endswith('map')][0]].tolist()
+    map1 = [0 if str(item) == 'nan' else item for item in map1]
 
-    plt.plot(xpoints, ypoints, 'o')
-    plt.show()
+    # Plot the difference between maps of col1 and col2
+    for word in list(infile_df.columns):
+        if word.startswith(col2) and word.endswith("map"):
+            map2 = [0 if str(item) == 'nan' else item for item in infile_df[word]]
+            diff = np.subtract(map2, map1)
+            plt.plot(diff, [d for d in range(len(map1))])
+            plt.show()
 
 
 def compare_mAP_all_row(infile_df, outfile_path, col1, col2):
-    headers = list(infile_df.columns)
-    map_columns = [word for word in headers if word.endswith("map")]
-    map1_header = [word for word in map_columns if word.startswith(col1)][0]
-    map2_header = [word for word in map_columns if word.startswith(col2)][0]
-
-    infile_df.dropna(subset=[map1_header, map2_header])
-
-    map1 = infile_df[[word for word in map_columns if word.startswith(col1)][0]].tolist()
-    map1 = [0 if str(item) == 'nan' else item for item in map1]
-    map2 = infile_df[[word for word in map_columns if word.startswith(col2)][0]].tolist()
-    map2 = [0 if str(item) == 'nan' else item for item in map2]
-
-    mean1 = np.mean(map1)
-    mean2 = np.mean(map2)
-
-    return mean1, mean2
-    output_file_name = f'compare.mAP.{col1}.{col2}.txt'
-    with open(outfile_path+output_file_name, 'w') as file:
-        file.write(f'{col1} mean of mAP is: {mean1}\n')
-        file.write(f'{col2} mean of mAP is: {mean2}\n')
+    with open(outfile_path + '.txt', 'w') as file:
+        for word in list(infile_df.columns):
+            if (word.startswith(col2) or word.startswith(col1)) and word.endswith("map"):
+                map = infile_df[word].tolist()
+                mean = np.mean([0 if str(item) == 'nan' else item for item in map])
+                file.write(f'{word} mean of mAP is: {mean}\n')
 
 
 def get_input():
@@ -97,10 +89,13 @@ def get_input():
     parser = optparse.OptionParser()
     parser.add_option('--column1',
                       action="store", dest="col1",
-                      help="query string", default="abstractqueryexpansion")
+                      help="Query string, the main column you want to do the comparison",
+                      default="abstractqueryexpansion")
     parser.add_option('--column2',
                       action="store", dest="col2",
-                      help="query string", default="backtranslation")
+                      help="Query string, Name of the expander or column(s) (this name can refer to multiple columns) "
+                           "with want to compare to the first column",
+                      default="backtranslation")
 
     options, args = parser.parse_args()
 
@@ -117,10 +112,8 @@ if __name__ == '__main__':
     infile_df = pd.read_csv(infile_path)
     infile_df = infile_df.reset_index()  # making sure indexes pair with number of rows
 
-    for index, each_col in enumerate(columns.col2):
-        compare_mAP_each_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=each_col)
-        compare_mAP_all_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=each_col)
+    outfile_path += f'compare.mAP.{columns.col1}.{columns.col2}'
 
-
-
-
+    # compare_mAP_each_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=columns.col2)
+    # compare_mAP_all_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=columns.col2)
+    plot_result(infile_df=infile_df, col1=columns.col1, col2=columns.col2)
