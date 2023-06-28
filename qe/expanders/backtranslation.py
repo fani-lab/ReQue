89d20cfe3abc44301954d1ed04fcc9c6fffbe410
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2023-06-28 19:40:24 -0400

    Add comments in backtranslation expander

--- qe/expanders/backtranslation.py
+++ qe/expanders/backtranslation.py
@@ -13,22 +13,18 @@ class BackTranslation(AbstractQExpander):
     def __init__(self, tgt):
         AbstractQExpander.__init__(self)
 
+        # Initialization
         self.tgt = tgt
-
         model = AutoModelForSeq2SeqLM.from_pretrained(param.backtranslation['model_card'])
         tokenizer = AutoTokenizer.from_pretrained(param.backtranslation['model_card'])
 
+        # Translation models
+        self.translator = pipeline("translation", model=model, tokenizer=tokenizer, src_lang=param.backtranslation['src_lng'], tgt_lang=self.tgt, max_length=param.backtranslation['max_length'], device=param.backtranslation['device'])
+        self.back_translator = pipeline("translation", model=model, tokenizer=tokenizer, src_lang=self.tgt, tgt_lang=param.backtranslation['src_lng'], max_length=param.backtranslation['max_length'], device=param.backtranslation['device'])
+        # Model use for calculating semsim
         self.transformer_model = SentenceTransformer(param.backtranslation['transformer_model'])
 
-        self.translator = pipeline("translation", model=model, tokenizer=tokenizer,
-                                   src_lang=param.backtranslation['src_lng'], tgt_lang=self.tgt,
-                                   max_length=param.backtranslation['max_length'],
-                                   device=param.backtranslation['device'])
-        self.back_translator = pipeline("translation", model=model, tokenizer=tokenizer, src_lang=self.tgt,
-                                        tgt_lang=param.backtranslation['src_lng'],
-                                        max_length=param.backtranslation['max_length'],
-                                        device=param.backtranslation['device'])
-
+    # Generate the backtranslated of the original query then calculates the difference of the two queries
     def get_expanded_query(self, q, args=None):
         translated_query = self.translator(q)
         back_translated_query = self.back_translator(translated_query[0]['translation_text'])
@@ -36,12 +32,12 @@ class BackTranslation(AbstractQExpander):
         score = self.semsim(q, back_translated_query[0]['translation_text'])
         return super().get_expanded_query(back_translated_query[0]['translation_text'], [score])
 
-        # score = self.semsim(q, q)
-        # return super().get_expanded_query(q, [score])
-
+    # Returns the name of the model ('backtranslation) with name of the target language
+    # Example: 'backtranslation_fra_latn'
     def get_model_name(self):
         return super().get_model_name() + '_' + self.tgt.lower()
 
+    # Calculate the difference between the original and back-translated query
     def semsim(self, q1, q2):
         me, you = self.transformer_model.encode([q1, q2])
         return 1 - cosine(me, you)
