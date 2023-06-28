e3028a5fe8d3561eb6eef83730b5fa940cbe7c7a
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2023-06-28 17:39:11 -0400

    Update param.py
    remove redundant comments

--- qe/cmn/param.py
+++ qe/cmn/param.py
@@ -7,23 +7,23 @@ ReQue = {
     'op':  ['generate', 'search', 'evaluate', 'build'],  # ['generate', 'search', 'evaluate', 'build']
     'expanders': {
         'SenseDisambiguation':  0,
-        'Thesaurus':            0,  # done-half
-        'Wordnet':              0,  # done
-        'Conceptnet':           0,  # done-half
-        'Tagmee':               0,  # done-half
+        'Thesaurus':            0,
+        'Wordnet':              0,
+        'Conceptnet':           0,
+        'Tagmee':               0,
 
-        'Word2Vec':             0,  # done
-        'Glove':                0,  # done
-        'Anchor':               0,  # xdone
-        'Wiki':                 0,  # xdone
+        'Word2Vec':             0,
+        'Glove':                0,
+        'Anchor':               0,
+        'Wiki':                 0,
 
-        'KrovetzStemmer':       0,  # done
-        'LovinsStemmer':        1,  # done
-        'PaiceHuskStemmer':     0,  # done
-        'PorterStemmer':        0,  # done
-        'Porter2Stemmer':       0,  # done
-        'SRemovalStemmer':      0,  # done
-        'Trunc4Stemmer':        0,  # done
+        'KrovetzStemmer':       0,
+        'LovinsStemmer':        0,
+        'PaiceHuskStemmer':     0,
+        'PorterStemmer':        0,
+        'Porter2Stemmer':       0,
+        'SRemovalStemmer':      0,
+        'Trunc4Stemmer':        0,
         'Trunc5Stemmer':        0,
 
         'RelevanceFeedback':    0,
@@ -35,18 +35,13 @@ ReQue = {
         'BertQE':               0,
         'RM3':                  0,
 
-        'BackTranslation':      0,
+        'BackTranslation':      1,
     }
 }
 
 anserini = {
     'path': '../anserini/',
     'trec_eval': '../anserini/eval/trec_eval.9.0.4/trec_eval'
-}
-
-pyserini = {
-    'path': '../pyserini/',
-    'trec_eval': '../pyserini/tools/eval/trec_eval.9.0.4/trec_eval'
 }
 
 corpora = {
@@ -66,7 +61,7 @@ corpora = {
         'size': 25000000,
         'topics': '../ds/gov2/{}.terabyte0{}.txt',  # {} is a placeholder for subtopics in main.py -> run()
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
-        'w_t': 4,  # OnFields
+        'w_t': 4,     # OnFields
         'w_a': 0.25,  # OnFields
         'tokens': 17000000000,
         'qrels': '../ds/gov2/qrels.terabyte0{}.txt',  # {} is a placeholder for subtopics in main.py -> run()
@@ -77,8 +72,8 @@ corpora = {
         'size': 50000000,
         'topics': '../ds/clueweb09b/topics.web.{}.txt',  # {} is a placeholder for subtopics in main.py -> run()
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
-        'w_t': 1,  # OnFields
-        'w_a': 0,  # OnFields
+        'w_t': 1,     # OnFields
+        'w_a': 0,     # OnFields
         'tokens': 31000000000,
         'qrels': '../ds/clueweb09b/qrels.web.{}.txt',  # {} is a placeholder for subtopics in main.py -> run()
         'extcorpus': 'gov2',  # AdaptOnFields
@@ -88,8 +83,8 @@ corpora = {
         'size': 50000000,
         'topics': '../ds/clueweb12b13/topics.web.{}.txt',  # {} is a placeholder for subtopics in main.py -> run()
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
-        'w_t': 4,  # OnFields
-        'w_a': 0,  # OnFields
+        'w_t': 4,     # OnFields
+        'w_a': 0,     # OnFields
         'tokens': 31000000000,
         'qrels': '../ds/clueweb12b13/qrels.web.{}.txt',  # {} is a placeholder for subtopics in main.py -> run()
         'extcorpus': 'gov2',  # AdaptOnFields
@@ -100,7 +95,7 @@ corpora = {
         'topics': '../ds/antique/topics.antique.txt',
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
         'w_t': 2.25,  # OnFields # to be tuned
-        'w_a': 1,  # OnFields # to be tuned
+        'w_a': 1,     # OnFields # to be tuned
         'tokens': 16000000,
         'qrels': '../ds/antique/qrels.antique.txt',
         'extcorpus': 'gov2',  # AdaptOnFields
@@ -112,7 +107,7 @@ corpora = {
         'topics': '../ds/trec09mq/09.mq.topics.20001-60000.prep',
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
         'w_t': 2.25,  # OnFields # to be tuned
-        'w_a': 1,  # OnFields # to be tuned
+        'w_a': 1,     # OnFields # to be tuned
         'tokens': 16000000,
         'qrels': '../ds/trec09mq/prels.20001-60000.prep',
         'extcorpus': 'gov2',  # AdaptOnFields
@@ -122,8 +117,8 @@ corpora = {
         'size': 4632359,
         'topics': '../ds/dbpedia/topics.dbpedia.txt',
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
-        'w_t': 1,  # OnFields # to be tuned
-        'w_a': 1,  # OnFields # to be tuned
+        'w_t': 1,     # OnFields # to be tuned
+        'w_a': 1,     # OnFields # to be tuned
         'tokens': 200000000,
         'qrels': '../ds/dbpedia/qrels.dbpedia.txt',
         'extcorpus': 'gov2',  # AdaptOnFields
@@ -135,7 +130,7 @@ corpora = {
         'topics': '../ds/orcas/preprocess/orcas-I-2M_topics.prep',
         'prels': '',  # this will be generated after a retrieval {bm25, qld}
         'w_t': 2.25,  # OnFields # to be tuned
-        'w_a': 1,  # OnFields # to be tuned
+        'w_a': 1,     # OnFields # to be tuned
         'tokens': 16000000,
         'qrels': '../ds/orcas/preprocess/orcas-doctrain-qrels.prep',
         'extcorpus': 'gov2',  # AdaptOnFields
@@ -143,11 +138,11 @@ corpora = {
 }
 
 backtranslation = {
-    'src_lng': 'eng_Latn',  #
-    'tgt_lng': ['fra_Latn'],# ['pes_Arab', 'yue_Hant', 'fra_Latn', 'deu_Latn', 'dan_Latn']
+    'src_lng': 'eng_Latn',
+    'tgt_lng': ['fra_Latn'],  # ['pes_Arab', 'yue_Hant', 'fra_Latn', 'deu_Latn', 'dan_Latn']
     'max_length': 512,
     'device': 'cpu',
-    'model_card': 'facebook/nllb-200-distilled-600M', #t5-base
+    'model_card': 'facebook/nllb-200-distilled-600M',
     'transformer_model': 'johngiorgi/declutr-small',
 }
 
