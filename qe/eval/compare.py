476a06a11c4198670c1289b065255b995a4a7ef9
Delaram Rajaei <48606206+DelaramRajaei@users.noreply.github.com>
2023-07-11 19:32:23 -0400

    Update compare.py

--- qe/eval/compare.py
+++ qe/eval/compare.py
@@ -4,18 +4,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import os
+import sys
+sys.path.extend(['../qe/output'])
 
-def calculate_tValue_cohensD(map1, map2):
+def calculate_tValue_cohensD(map1, map2, i):
     # Calculate the mean and standard deviation
     mean_diff = map2 - map1
     std_diff = np.sqrt((map1 ** 2 + map2 ** 2) / 2)
 
-    # Calculate Cohen's d
-    cohens_d = mean_diff / std_diff
+    if std_diff == 0:
+        cohens_d = 0
+        t_value = 0
+    else:
+        # Calculate Cohen's d
+        cohens_d = mean_diff / std_diff
 
-    n = 1  # Number of observations (in this case, each value is a single observation)
-    std_error = std_diff / np.sqrt(n)
-    t_value = mean_diff / std_error
+        n = 1  # Number of observations (in this case, each value is a single observation)
+        std_error = std_diff / np.sqrt(n)
+        t_value = mean_diff / std_error
 
     return t_value, cohens_d
 
@@ -42,12 +49,12 @@ def compare_mAP_each_row(infile_df, outfile_path, col1, col2):
     for index, row in infile_df.iterrows():
         # Initialize new line
         # If a list of names, starting with col1, is returned, only the first one is considered!
-        map1 = row[col1]
+        map1 = 0 if str(row[col1]) == 'nan' else float(row[col1])
         new_line = [row.qid, map1]
 
         for index, map2 in enumerate(col2_list):
-            # Check empty cells
-            t_value, cohens_d = calculate_tValue_cohensD(map1=map1, map2=row[map2])
+            m2 = 0 if str(row[map2]) == 'nan' else float(row[map2])
+            t_value, cohens_d = calculate_tValue_cohensD(map1=map1, map2=m2, i=index)
 
             # New row of the output file csv file
             new_line.append(row[map2])  # col 2 mAP
@@ -60,20 +67,22 @@ def compare_mAP_each_row(infile_df, outfile_path, col1, col2):
     csv_result.to_csv(outfile_path + '.csv')
 
 
-def plot_result(infile_df, col1, col2):
+def plot_result(infile_df, outfile, col1, col2):
     # Map of the main column
     map1 = infile_df[[word for word in list(infile_df.columns) if word.startswith(col1) and word.endswith('map')][0]].tolist()
-    map1 = [0 if str(item) == 'nan' else item for item in map1]
+    map1 = [0 if str(item) == 'nan' else float(item) for item in map1]
 
     # Plot the difference between maps of col1 and col2
     for word in list(infile_df.columns):
         if word.startswith(col2) and word.endswith("map"):
-            map2 = [0 if str(item) == 'nan' else item for item in infile_df[word]]
+            map2 = [0 if str(item) == 'nan' else float(item) for item in infile_df[word]]
             diff = np.subtract(map2, map1)  # Delta map
             plt.plot(diff, label=word.replace(word[word.find('.'):], ''))
 
     plt.legend()
-    plt.show()
+
+    # Saving the figure.
+    plt.savefig(outfile + ".jpg")
 
 
 def compare_mAP_all_row(infile_df, outfile_path, col1, col2):
@@ -98,23 +107,32 @@ def get_input():
                            "with want to compare to the first column",
                       default="backtranslation")
 
+
     options, args = parser.parse_args()
 
     return options
 
+def find_best_map(input_file):
+    #
+    with open(input_file, 'w') as file:
+        pass
 
 if __name__ == '__main__':
     columns = get_input()
 
     # Constant
-    infile_path = '../qe/output/robust04/topics.robust04.bm25.map.all.csv'
-    outfile_path = '../qe/output/compare/'
+    infile_path = '../output/gov2/topics.gov2.701-850.bm25.map.all.csv'
+    corpus = 'gov2'
+    outfile_path = '../output/compare/' + corpus + '/'
+
+    if not os.path.exists(outfile_path):
+        os.makedirs(outfile_path)
 
     infile_df = pd.read_csv(infile_path)
     infile_df = infile_df.reset_index()  # making sure indexes pair with number of rows
 
-    outfile_path += f'compare.mAP.{columns.col1}.{columns.col2}'
+    outfile_path += f'compare.{corpus}.mAP.{columns.col1}.{columns.col2}'
 
-    # compare_mAP_each_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=columns.col2)
-    # compare_mAP_all_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=columns.col2)
-    plot_result(infile_df=infile_df, col1=columns.col1, col2=columns.col2)
+    compare_mAP_each_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=columns.col2)
+    compare_mAP_all_row(infile_df=infile_df, outfile_path=outfile_path, col1=columns.col1, col2=columns.col2)
+    plot_result(infile_df=infile_df, outfile=outfile_path, col1=columns.col1, col2=columns.col2)
